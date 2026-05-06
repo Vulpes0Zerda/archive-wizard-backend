6d26a970192cfe4759c087447b189ece15306070
Lucy B. <lucyburock@gmail.com>
2026-05-06 18:08:40 +0200

    initilization

---
+++ src/test/java/de/htw_berlin/archive_wizard/ArchiveWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.archive_wizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
