d7ecae36ee91c4d65fb2a17aa96269ddfed2b2d0
Lucy B. <lucyburock@gmail.com>
2026-06-07 02:25:36 +0200

    created Entities according to the tabular ERD, added basic setters and getters and handled Object Methods

---
+++ src/main/java/de/htwberlin/archivewizard/category/group/CategoryGroup.java
@@ -0,0 +1,118 @@
+package de.htwberlin.archivewizard.category.group;
+
+// ArchiveWizard
+import de.htwberlin.archivewizard.shelf.Shelf;
+import de.htwberlin.archivewizard.category.key.CategoryKey;
+
+// Java
+import java.util.List;
+
+// JPA
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category_groups")
+public class CategoryGroup {
+
+  // ──────────────────────────────────────────────────────────────
+  // Attributes
+  // ──────────────────────────────────────────────────────────────
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_groups_seq")
+  @SequenceGenerator(name = "category_groups_seq", sequenceName = "category_groups_sequence",
+      allocationSize = 1)
+  @Column(name = "id", nullable = false, unique = true)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 80)
+  private String name;
+
+  @OneToMany(mappedBy = "categoryGroup")
+  private List<Shelf> shelfs;
+
+  @OneToMany(mappedBy = "categoryGroup")
+  private List<CategoryKey> categoryKeys;
+
+  // ──────────────────────────────────────────────────────────────
+  // Constructors
+  // ──────────────────────────────────────────────────────────────
+
+  protected CategoryGroup() {}
+
+  public CategoryGroup(String name) {
+    this.name = name;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Setters
+  // ──────────────────────────────────────────────────────────────
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Getters
+  // ──────────────────────────────────────────────────────────────
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public List<Shelf> getShelfs() {
+    return this.shelfs;
+  }
+
+  public List<CategoryKey> getCategoryKeys() {
+    return this.categoryKeys;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Object Methods
+  // ──────────────────────────────────────────────────────────────
+
+  @Override
+  public boolean equals(Object otherObject) {
+    if (this == otherObject) {
+      return true;
+    }
+    if (!(otherObject instanceof CategoryGroup)) {
+      return false;
+    }
+
+    final CategoryGroup otherCategoryGroup = (CategoryGroup) otherObject;
+
+    if (!otherCategoryGroup.getId().equals(this.getId())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 91 * this.getId().hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s - @%d[\n  id=%d, name='%s'\n]", this.getClass().getSimpleName(),
+        System.identityHashCode(this), getId(), getName());
+  }
+
+}
---
+++ src/main/java/de/htwberlin/archivewizard/category/key/CategoryKey.java
@@ -0,0 +1,132 @@
+package de.htwberlin.archivewizard.category.key;
+
+// ArchiveWizard
+import de.htwberlin.archivewizard.category.group.CategoryGroup;
+import de.htwberlin.archivewizard.category.value.CategoryValue;
+
+// Java
+import java.util.List;
+
+// JPA
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category_keys")
+public class CategoryKey {
+
+  // ──────────────────────────────────────────────────────────────
+  // Attributes
+  // ──────────────────────────────────────────────────────────────
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_key_seq")
+  @SequenceGenerator(name = "category_key_seq", sequenceName = "category_key_sequence",
+      allocationSize = 1)
+  @Column(name = "id", nullable = false, unique = true)
+  private long id;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "belongs_to_category_group", nullable = false)
+  private CategoryGroup categoryGroup;
+
+  @Column(name = "key", nullable = false, length = 80)
+  private String key;
+
+  @OneToMany(mappedBy = "categoryKey", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<CategoryValue> categoryValues;
+
+  // ──────────────────────────────────────────────────────────────
+  // Constructors
+  // ──────────────────────────────────────────────────────────────
+
+  protected CategoryKey() {}
+
+  public CategoryKey(String key, CategoryGroup categoryGroup) {
+    this.key = key;
+    this.categoryGroup = categoryGroup;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Setters
+  // ──────────────────────────────────────────────────────────────
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public void setKey(final String key) {
+    this.key = key;
+  }
+
+  public void setCategoryGroup(final CategoryGroup categoryGroup) {
+    this.categoryGroup = categoryGroup;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Getters
+  // ──────────────────────────────────────────────────────────────
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public String getKey() {
+    return this.key;
+  }
+
+  public CategoryGroup getCategoryGroup() {
+    return this.categoryGroup;
+  }
+
+  public List<CategoryValue> getCategoryValues() {
+    return this.categoryValues;
+  }
+
+  // ──────────────────────────────────────────────────────────────
+  // Object Methods
+  // ──────────────────────────────────────────────────────────────
+
+  @Override
+  public boolean equals(Object otherObject) {
+    if (this == otherObject) {
+      return true;
+    }
+    if (!(otherObject instanceof CategoryKey)) {
+      return false;
+    }
+
+    final CategoryKey otherCategoryKey = (CategoryKey) otherObject;
+
+    if (!otherCategoryKey.getId().equals(this.getId())) {
+      return false;
+    }
+    if (!otherCategoryKey.getCategoryGroup().equals(this.getCategoryGroup())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 91 * this.getCategoryGroup().hashCode() + this.getId().hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s - @%d[\n  id=%d, key='%s', categoryGroup={\n    %s\n  }\n]",
+        this.getClass().getSimpleName(), System.identityHashCode(this), getId(), getKey(),
+        getCategoryGroup().toString());
+  }
+
+}
