92afa84b8376184bd38090fdcbe409c12a954a29
Lucy B. <lucyburock@gmail.com>
2026-09-22 21:45:24 +0200

    added template category groups and keys, so that dll-auto can be switched to update

--- src/main/java/de/htwberlin/archivewizard/category/group/CategoryGroupRepository.java
+++ src/main/java/de/htwberlin/archivewizard/category/group/CategoryGroupRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryGroupRepository extends JpaRepository<CategoryGroup, Long> {
   List<CategoryGroup> findByUserOrUserIsNull(User user);
+
+  List<CategoryGroup> findAllByUserIsNull();
 }
--- src/main/java/de/htwberlin/archivewizard/category/key/CategoryKeyRepository.java
+++ src/main/java/de/htwberlin/archivewizard/category/key/CategoryKeyRepository.java
@@ -1,7 +1,12 @@
 package de.htwberlin.archivewizard.category.key;
 
+import de.htwberlin.archivewizard.category.group.CategoryGroup;
+
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryKeyRepository extends JpaRepository<CategoryKey, Long> {
+  public List<CategoryKey> getByCategoryGroup(CategoryGroup categoryGroup);
 
 }
